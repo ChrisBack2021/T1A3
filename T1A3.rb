b5d4145cc0b23cff21558ee7da4fd4ba0f34ae4c
Chris Back <chrisback011996@gmail.com>
2022-03-28 16:15:52 +1100

    Completed the adding function for food and calorie tracker.

--- T1A3.rb
+++ T1A3.rb
@@ -1,5 +1,4 @@
 require 'tty-prompt'
-require 'text-table'
 require 'tty-table'
 
 
@@ -17,35 +16,52 @@ puts 'Welcome to the fitness guru app, where we track your dreams for you'
 
 # First parameter is the message of the prompt. Second parameter is the list of options, which is the navigation array
 # user_selection = prompt.select("Please choose an option from the list", navigation)
-module Food
 
+module Food
     def self.food_tracker
         puts 'What food did you eat?'
         foods = gets.chomp
         puts "How many calories was in the #{foods}?"
         calories = gets.chomp.to_i
-
         return foods, calories
     end
-
 end
 
-food_and_cal_storage = [["Food", "Calories"]]
+def tracker
+    user_quit = false
+        until user_quit == true
+            food_cal_pairing = Food.food_tracker
+            puts 'Would you like to add in more food or quit?'
+            user_quit = gets.chomp
+            if user_quit == "quit"
+                user_quit = true
+            end
+        end
+    return food_cal_pairing
+end
 
+table = TTY::Table.new(["Food","Calories"], [tracker])
+puts table.render(:ascii)
 
-food_and_cal_storage.push(Food.food_tracker)
-food_cal_pairing = food_and_cal_storage.to_h
-
-puts food_cal_pairing.to_table(:first_row_is_head => true)
-
-
-
-
-
-
-
-
-
+user_continue = true
+while user_continue == true
+    puts 'If there is something you would like to remove, please type "remove".'
+    puts 'If you would like to add something, please type "add".'
+    puts 'If there is nothing to change, please type "exit".'
+    user_edits = gets.chomp
+    case user_edits
+    when 'add'
+        table << tracker
+        puts table.render(:ascii)
+    when 'remove'
+        puts 'removed testing'
+    # Below may need to be changed when you add in new features.
+    when 'exit'
+        user_continue = false
+    else
+        puts "Invalid choice. Please select from remove, add or next."
+   end
+end
 
 
 
@@ -54,8 +70,8 @@ puts food_cal_pairing.to_table(:first_row_is_head => true)
 # end
 
 # case user_selection
-#     when "1. Track your food intake and calorie intake"
-#         food_tracker
-#     when "2. Input your workouts and get a randomized list of workouts YOU choose."
-#         workout
+# when "1. Track your food intake and calorie intake"
+#    food_tracker
+# when "2. Input your workouts and get a randomized list of workouts YOU choose."
+#    workout
 # end
