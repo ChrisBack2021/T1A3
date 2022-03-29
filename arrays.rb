82dc92aeb875d7b6d64facc9cca8ee5d27de83ac
Chris Back <chrisback011996@gmail.com>
2022-03-29 19:20:16 +1100

    Fixed up removing from array for tables. Before removed entire table, now only last element in array

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+arr = [1,2,3]
+arr2 = [4,5,6]
+arr3 = arr + arr2
+
+p arr3
--- testfile.rb
+++ testfile.rb
@@ -12,8 +12,6 @@ module Food
     end
 end
 
-
-
 prompt = TTY::Prompt.new
 
 puts 'Welcome to the fitness guru app, where we track your dreams for you'
@@ -27,40 +25,41 @@ navigation = [
 # First parameter is the message of the prompt. Second parameter is the list of options, which is the navigation array
 user_selection = prompt.select("Please choose an option from the list", navigation)
 
-# nested loops inside function.
-def tracker
-    food_cal_pairing = Food.food_tracker
-end
 
+food_cal_pairing = []
 
-    # Ruby gems
-    table = TTY::Table.new(["Food", "Calories"], [tracker])
+    # Ruby gems for table
+def tables(input_values)
+    table = TTY::Table.new(["Food", "Calories"], (input_values))
     puts table.render(:ascii)
+end
     # CRUD
 
     user_continue = true
     while user_continue == true
         puts 'If you would like to add something, please type "add".'
-        puts 'If you would like to restart the table, please type "remove".'
+        puts 'To remove the last item, please type "remove".'
         puts 'If you wish to export the table to a csv, please type "csv".'
         puts 'If there is nothing to change, please type "exit".'
         user_edits = gets.chomp.strip.downcase
         case user_edits
         when 'add'
-            table << Food.food_tracker
-            puts table.render(:ascii)
+            food_cal_pairing << Food.food_tracker
+            tables(food_cal_pairing)
         when 'remove'
-            table_clear = table.drop_while { |x| x < table.length - 1 }
-            table_clear = TTY::Table.new(["Food", "Calories"], [])
-            puts table2.render(:ascii)
-            table = table_clear
-
+            food_cal_pairing.delete_at(food_cal_pairing.length - 1)
+            tables(food_cal_pairing)
         # File handling
         when 'csv'
+            table = tables(food_cal_pairing)
+            p table
+            # empty_array = []
+            # csv_array = (tables(food_cal_pairing) + empty_array)
+
             CSV.open('food_tracker.csv', 'a') do |csv|
                 csv << [table]
-            end
             puts 'The file has been exported to food_tracker.csv'
+            end
         when 'exit'
             user_continue = false
         else
@@ -70,15 +69,9 @@ end
 
 
 module Workout
-    
     def workout
         puts "Please list a minimum of 5 exercises to a maximum of 15 exercises one by one."
     end
-
-
-
-
-
 
 case user_selection
 when "1. Track your food intake and calorie intake"
